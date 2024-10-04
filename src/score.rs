2a2a220d19eeed41e8fc4406d129a5710ba4e998
Daniel Pettersson <daniel.g.pettersson@gmail.com>
2024-10-04 15:03:15 +0200

    Add game over text

--- src/score.rs
+++ src/score.rs
@@ -1,7 +1,11 @@
 use crate::{FontAssets, GameState};
 use bevy::app::App;
 use bevy::math::Vec2;
-use bevy::prelude::{default, in_state, Camera, Color, Commands, Component, Entity, FixedUpdate, GlobalTransform, IntoSystemConfigs, JustifyText, Local, OnEnter, OnExit, Plugin, Query, Res, ResMut, Resource, Text, Text2dBundle, TextStyle, Transform, Update, With};
+use bevy::prelude::{
+    default, in_state, Camera, Color, Commands, Component, Entity, FixedUpdate, GlobalTransform,
+    IntoSystemConfigs, JustifyText, Local, OnEnter, OnExit, Plugin, Query, Res, ResMut, Resource,
+    Text, Text2dBundle, TextStyle, Transform, Update, Vec3, With,
+};
 use bevy::sprite::Anchor;
 
 pub struct ScorePlugin;
@@ -15,7 +19,9 @@ impl Plugin for ScorePlugin {
             .add_systems(
                 FixedUpdate,
                 update_score.run_if(in_state(GameState::InGame)),
-            );
+            )
+            .add_systems(OnEnter(GameState::GameOver), create_game_over)
+            .add_systems(OnExit(GameState::GameOver), remove_score_text);
     }
 }
 
@@ -25,13 +31,18 @@ pub struct Score(pub u32);
 #[derive(Component)]
 pub struct ScoreText;
 
-fn create_score_text(mut commands: Commands, fonts: Res<FontAssets>, mut score: ResMut<Score>, query_camera: Query<(&Camera, &GlobalTransform)>,) {
+fn create_score_text(
+    mut commands: Commands,
+    fonts: Res<FontAssets>,
+    mut score: ResMut<Score>,
+    query_camera: Query<(&Camera, &GlobalTransform)>,
+) {
     score.0 = 0;
     let (camera, camera_transform) = query_camera.single();
     let score_pos = camera
         .viewport_to_world_2d(camera_transform, Vec2::new(10., 10.))
         .unwrap_or(Vec2::ZERO);
-    
+
     commands.spawn((
         Text2dBundle {
             text: Text::from_section(
@@ -51,10 +62,7 @@ fn create_score_text(mut commands: Commands, fonts: Res<FontAssets>, mut score: 
     ));
 }
 
-fn remove_score_text(
-    mut commands: Commands,
-    query_score_text: Query<Entity, With<ScoreText>>,
-) {
+fn remove_score_text(mut commands: Commands, query_score_text: Query<Entity, With<ScoreText>>) {
     for entity in query_score_text.iter() {
         commands.entity(entity).despawn();
     }
@@ -68,7 +76,7 @@ fn scroll_score(
     let score_pos = camera
         .viewport_to_world_2d(camera_transform, Vec2::new(10., 10.))
         .unwrap_or(Vec2::ZERO);
-    
+
     for mut transform in score_query.iter_mut() {
         transform.translation = score_pos.extend(10.);
     }
@@ -85,4 +93,42 @@ fn update_score(
             text.sections[0].value = format!("{}", score.0);
         }
     }
+}
+
+fn create_game_over(mut commands: Commands, fonts: Res<FontAssets>, mut score: ResMut<Score>) {
+    commands.spawn((
+        Text2dBundle {
+            text: Text::from_section(
+                format!("Game Over\nScore {}", score.0),
+                TextStyle {
+                    font: fonts.segmental.clone(),
+                    font_size: 70.0,
+                    color: Color::srgb(2.0, 2.0, 0.0),
+                },
+            )
+            .with_justify(JustifyText::Center),
+            text_anchor: Anchor::Center,
+            transform: Transform::from_translation(Vec3::new(0., 50., 0.)),
+            ..default()
+        },
+        ScoreText,
+    ));
+
+    commands.spawn((
+        Text2dBundle {
+            text: Text::from_section(
+                "Press any key to play again",
+                TextStyle {
+                    font: fonts.segmental.clone(),
+                    font_size: 30.0,
+                    color: Color::srgb(1.5, 1.5, 1.5),
+                },
+            )
+            .with_justify(JustifyText::Center),
+            text_anchor: Anchor::Center,
+            transform: Transform::from_translation(Vec3::new(0., -50., 0.)),
+            ..default()
+        },
+        ScoreText,
+    ));
 }
